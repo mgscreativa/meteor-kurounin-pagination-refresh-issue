ff22456e2c745f97fa858b283bd6ff29ac5d5eb3
kurounin <wiseon3@yahoo.com>
2017-07-28 10:37:07 +0300

    Fix issue with tracker invalidating subscription.

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Meteor } from 'meteor/meteor';
+import { Tracker } from 'meteor/tracker';
+import { Roles } from 'meteor/alanning:roles';
 import App from '../../ui/layouts/App/App';
 
 import '../../ui/stylesheets/app.scss';
 
-Meteor.startup(() => render(<App />, document.getElementById('react-root')));
+Meteor.startup(() => {
+  Tracker.autorun((c) => {
+    if (Meteor.loggingIn() || !Roles.subscription.ready()) {
+      return;
+    }
+
+    c.stop();
+    Meteor.defer(() => render(<App />, document.getElementById('react-root')));
+  });
+});
